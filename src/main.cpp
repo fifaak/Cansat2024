7e2bc063091778772454aae00dc1b6359a1632fa
FIFA AK <pataradany@gmail.com>
2024-03-21 21:42:48 +0700

    fixed some bugs

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,15 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Servo.h>
+
 Adafruit_MPU6050 mpu;
 Servo deployServo;
-const float Lunch_threshold = 20.0; // G force threshold from all axis
+const float Lunch_threshold = 2.0; // G force threshold from all axis
 bool Lunch_state = false;
 bool Apogee_state = false;
 bool Normal_eject = false;
 bool Emergency_eject = false;
 unsigned long start_time; // Variable to store the start time
-
 
 void Check_module(){
     // MPU6050
@@ -27,17 +27,17 @@ void Check_module(){
 void Eject() {
   deployServo.write(0); 
 }
-void Lunching(sensors_event_t a,bool Normal_eject, bool Emergency_eject){
-  while(!(Normal_eject || Emergency_eject)){
-    if ((millis() - start_time)>10000){
-      Emergency_eject = true;
-      Normal_eject = false;
+void Lunching(sensors_event_t a, bool* pNormal_eject, bool* pEmergency_eject){
+  while(!(*pNormal_eject || *pEmergency_eject)){
+    if ((millis() - start_time) > 10000){
       Eject();
+      *pEmergency_eject = true;
+      *pNormal_eject = false;
     }
-    int angle= sqrt(a.acceleration.x * a.acceleration.x +a.acceleration.y * a.acceleration.y);
+    int angle = sqrt(a.acceleration.x * a.acceleration.x + a.acceleration.y * a.acceleration.y);
     // Serial.println("Lunching:");
     delay(100);
-    Serial.println((millis() - start_time)/1000);
+    Serial.println((millis() - start_time) / 1000);
   }
 }
 void setup() {
@@ -46,20 +46,22 @@ void setup() {
   deployServo.attach(9); // Servo attached to pin 9
   deployServo.write(90);  // Set Servo to start position
 }
+
 void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);  
-  float acceleration = sqrt(a.acceleration.x * a.acceleration.x +a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z);
+  float acceleration = sqrt(a.acceleration.x * a.acceleration.x + a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z);
   Serial.println(acceleration);
+  
   if (acceleration >= Lunch_threshold) {
     Lunch_state = true;
     start_time = millis(); // Initialize start time
-    Lunching(a,false,false);
+    Lunching(a, &Normal_eject, &Emergency_eject);
     while(1){
-      if (Emergency_eject)   Serial.println("Emergency_eject");
-      else Serial.println("Normal_eject");
+      if (Emergency_eject) Serial.println("Emergency_eject");
+      else  Serial.println("Normal_eject");
       delay(500);
     }
   }
-    delay(100); // Delay for stability
+  delay(100); // Delay for stability
 }
