8859d527a1473f0c3aa696894bc326b96769c83a
FIFA AK <pataradany@gmail.com>
2024-03-21 21:34:38 +0700

    condition upgraded

--- src/main.cpp
+++ src/main.cpp
@@ -3,49 +3,63 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Servo.h>
-
 Adafruit_MPU6050 mpu;
 Servo deployServo;
+const float Lunch_threshold = 20.0; // G force threshold from all axis
+bool Lunch_state = false;
+bool Apogee_state = false;
+bool Normal_eject = false;
+bool Emergency_eject = false;
+unsigned long start_time; // Variable to store the start time
 
-// Threshold acceleration for deployment
-const float deployThreshold = 30.0; // Adjust as needed
 
+void Check_module(){
+    // MPU6050
+    if (!mpu.begin()) {
+      Serial.println("Failed to find MPU6050 chip");
+      while (1) {
+        delay(10);
+      }
+    }
+    Serial.println("Found MPU6050");
+    // END MPU6050
+}
+void Eject() {
+  deployServo.write(0); 
+}
+void Lunching(sensors_event_t a,bool Normal_eject, bool Emergency_eject){
+  while(!(Normal_eject || Emergency_eject)){
+    if ((millis() - start_time)>10000){
+      Emergency_eject = true;
+      Normal_eject = false;
+      Eject();
+    }
+    int angle= sqrt(a.acceleration.x * a.acceleration.x +a.acceleration.y * a.acceleration.y);
+    // Serial.println("Lunching:");
+    delay(100);
+    Serial.println((millis() - start_time)/1000);
+  }
+}
 void setup() {
   Serial.begin(115200);
-  
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
-
+  Check_module();
   deployServo.attach(9); // Servo attached to pin 9
-  deployServo.write(90); 
+  deployServo.write(90);  // Set Servo to start position
 }
-
 void loop() {
   sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-  
-  // Calculate total acceleration magnitude
+  mpu.getEvent(&a, &g, &temp);  
   float acceleration = sqrt(a.acceleration.x * a.acceleration.x +a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z);
   Serial.println(acceleration);
-  if (acceleration >= deployThreshold) {
-    deployServo.write(0); // Assuming 180 opens the servo
-
-    delay(1000); // Assuming 1 second is enough for deployment
-    
+  if (acceleration >= Lunch_threshold) {
+    Lunch_state = true;
+    start_time = millis(); // Initialize start time
+    Lunching(a,false,false);
+    while(1){
+      if (Emergency_eject)   Serial.println("Emergency_eject");
+      else Serial.println("Normal_eject");
+      delay(500);
     }
-    
+  }
     delay(100); // Delay for stability
 }
-
-// void deployCanSat() {
-//   // Open the servo to release the CanSat
-//   deployServo.write(180); // Assuming 180 opens the servo
-//   delay(1000); // Assuming 1 second is enough for deployment
-  
-//   // Close the servo after deployment (optional)
-//   deployServo.write(0); // Assuming 0 closes the servo
-// }
